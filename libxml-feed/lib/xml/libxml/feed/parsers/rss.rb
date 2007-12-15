ff5d7ef32921a1bdf8bb7023d9f209bd562a33a9
erikh <erikh>
2007-12-15 07:10:56 +0000

    * Missing and duplicate tests for title/description/link in channel

--- libxml-feed/lib/xml/libxml/feed/parsers/rss.rb
+++ libxml-feed/lib/xml/libxml/feed/parsers/rss.rb
@@ -15,6 +15,13 @@ module XML
                         validation_error "Document Version does not exist"     unless @doc.root["version"]
                         validation_error "Document Version is not '2.0'"       unless @doc.root["version"] == "2.0"
                         validation_error "Channel elements do not exist"      if @doc.find('/rss/channel').length == 0
+
+                        @doc.find('/rss/channel').each_with_index do |node, i|
+                            %w(title link description).each do |x|
+                                validation_error "Channel node #{i} does not have a #{x} element" unless node.find('./' + x).length > 0
+                                validation_error "Channel node #{i} has more than one #{x} element" if node.find('./' + x).length > 1
+                            end
+                        end
                     else
                         validation_error "Validating RSS version #{@version} is not supported (yet!)"
                     end
--- libxml-feed/test/test_rss_parser.rb
+++ libxml-feed/test/test_rss_parser.rb
@@ -2,11 +2,14 @@ require 'test/unit'
 require 'xml/libxml/feed'
 
 class TestParserInterface < Test::Unit::TestCase
-    def test_validate_rss_20
+
+    def test_validate_rss
         assert_raise(XML::Feed::Parser::ValidationError) do
             XML::Feed::Parser.rss('1.0', File.open('test/data/valid-2.0-rss.xml'), true)
         end
+    end
 
+    def test_validate_rss_20
         assert_raise(XML::Feed::Parser::ValidationError) do
             XML::Feed::Parser.rss('2.0', File.open('test/data/basic.xml'), true)
         end
@@ -34,6 +37,36 @@ class TestParserInterface < Test::Unit::TestCase
         # missing channel 
         assert_raise(XML::Feed::Parser::ValidationError) do
             XML::Feed::Parser.rss('2.0', File.open('test/data/missing-channel-2.0-rss.xml'), true)
+        end
+        
+        # missing channel title
+        assert_raise(XML::Feed::Parser::ValidationError) do
+            XML::Feed::Parser.rss('2.0', File.open('test/data/missing-channel-title-2.0-rss.xml'), true)
+        end
+        
+        # missing channel link 
+        assert_raise(XML::Feed::Parser::ValidationError) do
+            XML::Feed::Parser.rss('2.0', File.open('test/data/missing-channel-link-2.0-rss.xml'), true)
+        end
+        
+        # missing channel description 
+        assert_raise(XML::Feed::Parser::ValidationError) do
+            XML::Feed::Parser.rss('2.0', File.open('test/data/missing-channel-description-2.0-rss.xml'), true)
+        end
+
+        # duplicate channel title 
+        assert_raise(XML::Feed::Parser::ValidationError) do
+            XML::Feed::Parser.rss('2.0', File.open('test/data/duplicate-channel-title-2.0-rss.xml'), true)
+        end
+
+        # duplicate channel link 
+        assert_raise(XML::Feed::Parser::ValidationError) do
+            XML::Feed::Parser.rss('2.0', File.open('test/data/duplicate-channel-link-2.0-rss.xml'), true)
+        end
+        
+        # duplicate channel description 
+        assert_raise(XML::Feed::Parser::ValidationError) do
+            XML::Feed::Parser.rss('2.0', File.open('test/data/duplicate-channel-description-2.0-rss.xml'), true)
         end
     end
 end
