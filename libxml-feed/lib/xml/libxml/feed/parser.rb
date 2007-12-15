a4664c0c0859b6c11fc21db5607ebbe0c004b18e
erikh <erikh>
2007-12-15 05:55:24 +0000

    * Removed experimental AST. Going to take a different approach.

--- libxml-feed/lib/xml/libxml/feed/parser.rb
+++ libxml-feed/lib/xml/libxml/feed/parser.rb
@@ -44,42 +44,6 @@ module XML
                     end
                 end
             end
-
-            class AST
-                
-                class TypeError < Exception
-                end
-
-                attr_reader :name
-                attr_reader :arguments
-                attr_reader :attributes
-
-                def initialize(symbol_name)
-                    @name = symbol_name
-                    @arguments = Array.new
-                    @attributes = Hash.new
-                end
-
-                def set_attribute(key, value)
-                    if key.kind_of? String and value.respond_to? :to_s
-                        @attributes[key] = value
-                    else
-                        raise TypeError, "Both key and value must be string. Value must at least be able to coerce into string."
-                    end
-                end
-
-                def add_ast(ast)
-                    if ast.kind_of? self.class
-                        @arguments.push ast
-                    else
-                        raise TypeError, "Must be an AST type to continue"
-                    end
-                end
-
-                def inspect
-                    return "<AST #{@name}: [arguments: #{@arguments.collect { |x| x.inspect }.join(" : ")}] - [ attributes: #{@attributes.collect { |key, value| "#{key}:#{value}" }.join("/")}]>"
-                end
-            end
         end
     end
 end
