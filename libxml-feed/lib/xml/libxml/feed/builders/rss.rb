63c74e51a73fffcdeec60bf79ad58220a4e26265
erikh <erikh>
2007-12-16 15:57:58 +0000

    * Builder now handles the originating document from the parser or creates a stub

--- libxml-feed/lib/xml/libxml/feed/builders/rss.rb
+++ libxml-feed/lib/xml/libxml/feed/builders/rss.rb
@@ -1,6 +1,30 @@
 class XML::Feed::Builder::Rss
+
+    attr_accessor :version
+    attr_accessor :doc
+
     def initialize(version, parser=nil)
         @version = version
         @parser  = parser
+
+        build_document
+    end
+
+    def to_xml
+        @doc.to_s
+    end
+
+    protected
+
+    def build_document
+        @doc = XML::Document.new
+
+        if @parser
+            @doc = @parser.doc
+        else
+            @doc.root = XML::Node.new("rss")
+        end
+
+        @doc.root["version"] = @version
     end
 end
--- libxml-feed/test/test_builder_interface.rb
+++ libxml-feed/test/test_builder_interface.rb
@@ -26,4 +26,26 @@ class TestBuilderInterface < Test::Unit::TestCase
 
         assert_instance_of(XML::Feed::Builder::Rss, builder)
     end
+
+    def test_output
+        builder = nil
+
+        assert_nothing_raised do
+            builder = XML::Feed::Builder.rss("2.0")
+        end
+
+        assert_equal("<?xml version=\"1.0\"?>\n<rss version=\"2.0\"/>\n", builder.to_xml)
+
+        parser = nil
+
+        assert_nothing_raised do
+            parser = XML::Feed::Parser.rss('2.0', File.open('test/data/valid-2.0-rss.xml'), true)
+        end
+
+        assert_nothing_raised do
+            builder = XML::Feed::Builder.rss("2.0", parser)
+        end
+
+        assert_equal(parser.doc.to_s, builder.to_xml)
+    end
 end
